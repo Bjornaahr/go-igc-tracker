4b77a4b521853075bc651cfbf193f90856fc1db2
Bjornar <bjornar.hammerseth@gmail.com>
2018-09-29 15:40:26 +0200

    Testing

--- main.go
+++ main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/marni/goigc"
 )
 
-func main() {
+func pilotHandler(w http.ResponseWriter, r *http.Request) {
 	s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
 	track, err := igc.ParseLocation(s)
 	if err != nil {
-		fmt.Errorf("Problem reading the track", err)
+		status := 400
+		http.Error(w, http.StatusText(status), status)
+		return
 	}
 
-	fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
+	fmt.Fprintf(w, "Pilot: %s, gliderType: %s, date: %s",
 		track.Pilot, track.GliderType, track.Date.String())
+}
+
+func main() {
+
 }
