08ae1414514f5f420b6f45449aab6e7e87b917f1
Bjornaahr <bjornar.hammerseth@gmail.com>
2018-10-09 22:49:57 +0200

    MetaInfo added

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/marni/goigc"
+	"github.com/p3lim/iso8601"
 )
+
+//MetaInfo about the program
+type MetaInfo struct {
+	Uptime  string `json:"uptime"`
+	Info    string `json:"info"`
+	Version string `json:"version"`
+}
+
+var startTime time.Time
+
+func init() {
+	startTime = time.Now()
+}
 
 func handlerPilot(w http.ResponseWriter, r *http.Request) {
 	s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
@@ -20,29 +36,40 @@ func handlerPilot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, track.Pilot)
 }
 
-func handlerIGCINFO(w http.ResponseWriter, r *http.Request) {
-	status := 404
-	http.Error(w, http.StatusText(status), status)
+func handlerAPI(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+
+	case ("GET"):
+		info := MetaInfo{iso8601.Format(time.Since(startTime)),
+			"Service for IGC tracks",
+			"1.0"}
+		infoJSON, err := json.Marshal(info)
+		if err != nil {
+			panic(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(infoJSON)
+
+	case ("POST"):
+
+	}
 }
 
-func handlerRubbish(w http.ResponseWriter, r *http.Request) {
-	status := 404
-	http.Error(w, http.StatusText(status), status)
-}
-
-//GetPort retrives the port
+//GetPort retrives the port from the enviorment
 func GetPort() string {
+	//Gets the port
 	var port = os.Getenv("PORT")
 	// Set a default port if there is nothing in the environment
 	if port == "" {
-		port = "4747"
+		port = "5000"
 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
 	}
 	return ":" + port
 }
 
 func main() {
-	http.HandleFunc("/igcinfo/", handlerIGCINFO)
-	http.HandleFunc("/rubbish/", handlerRubbish)
+	http.HandleFunc("/API/", handlerAPI)
 	http.ListenAndServe(GetPort(), nil)
 }
