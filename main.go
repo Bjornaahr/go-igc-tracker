388ed2454d783fe0de16e2817e676473fd9b6cfc
Bjornaahr <bjornar.hammerseth@gmail.com>
2018-10-10 19:57:32 +0200

    Added /api/igc POST and GET

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ import (
 	"github.com/p3lim/iso8601"
 )
 
+const (
+	VERSION = "1.0"
+	DESC    = "Service for IGC tracks."
+)
+
 //MetaInfo about the program
 type MetaInfo struct {
 	Uptime  string `json:"uptime"`
@@ -18,10 +23,24 @@ type MetaInfo struct {
 	Version string `json:"version"`
 }
 
+//Glider track info
+type Track struct {
+	ID          int       `json: "ID"`
+	Hdate       time.Time `json:"H_date"`
+	Pilot       string    `json:"pilot"`
+	Glider      string    `json:"glider"`
+	GliderID    string    `json:"glider_id"`
+	Tracklength string    `json:"track_length"`
+}
+
 var startTime time.Time
+var tracks map[int]Track
+var ID int
 
 func init() {
 	startTime = time.Now()
+	tracks = make(map[int]Track)
+	ID = 1
 }
 
 func handlerPilot(w http.ResponseWriter, r *http.Request) {
@@ -38,22 +57,56 @@ func handlerPilot(w http.ResponseWriter, r *http.Request) {
 
 func handlerAPI(w http.ResponseWriter, r *http.Request) {
 
-	switch r.Method {
+	info := MetaInfo{iso8601.Format(time.Since(startTime)),
+		DESC,
+		VERSION}
+	infoJSON, err := json.Marshal(info)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(infoJSON)
 
+}
+
+func handlerIGC(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
 	case ("GET"):
-		info := MetaInfo{iso8601.Format(time.Since(startTime)),
-			"Service for IGC tracks",
-			"1.0"}
-		infoJSON, err := json.Marshal(info)
+		IDS := []int{}
+
+		for id := range tracks {
+			IDS = append(IDS, id)
+		}
+		IDJSON, err := json.Marshal(IDS)
 		if err != nil {
-			panic(err)
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(IDJSON)
+
+	//Creates a track
+	case ("POST"):
+		var url string
+		err := json.NewDecoder(r.Body).Decode(&url)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		track, err := igc.ParseLocation(url)
+		tracks[ID] = Track{ID, track.Date, track.Pilot, track.GliderType, track.GliderID, "Length"}
+
+		infoJSON, err := json.Marshal(tracks[ID].ID)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
 		}
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write(infoJSON)
-
-	case ("POST"):
-
+		ID++
 	}
 }
 
@@ -70,6 +123,7 @@ func GetPort() string {
 }
 
 func main() {
-	http.HandleFunc("/API/", handlerAPI)
+	http.HandleFunc("/api/", handlerAPI)
+	http.HandleFunc("/api/igc", handlerIGC)
 	http.ListenAndServe(GetPort(), nil)
 }
